8b0dacd8340d49db9d325c7b76b0917aab3871eb
Alejandro-PC\Alejandro <mon17014@uvg.edu.gt>
2018-03-25 18:58:39 -0600

    Lectura de lista

--- Colas con prioridad/src/Main.java
+++ Colas con prioridad/src/Main.java
@@ -1,4 +1,8 @@
 //Paquetes
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 /**
@@ -10,5 +14,30 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		Scanner teclado = new Scanner(System.in);
+		String expresion = "";
+		try {
+			
+			/*Abrimos el archivo de texto*/
+			FileInputStream archivo = new FileInputStream("lista.txt");
+			/*Objeto de entrada*/
+			DataInputStream entrada = new DataInputStream(archivo);
+			/*Buffer, que servira para lectura*/
+			BufferedReader buffer = new BufferedReader(new InputStreamReader(entrada));
+			/*Leer archivo*/
+			String linea;
+			while ((linea = buffer.readLine()) != null) {
+				String[] temp = linea.split(", ");
+                String nombre = temp[0];
+                String descrip = temp[1];
+                String code = temp[2];
+            }
+			
+			entrada.close();
+		
+		} 
+		catch (Exception e) {
+			/*Mensaje de error*/
+			System.err.println("Ocurrio un error " + e.getMessage());
+		}
 	}
 }
